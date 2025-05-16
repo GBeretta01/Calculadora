4c6a2b1f8eab4e618478f5e47f495d2b817e91ac
Beretta <95521297+GBeretta01@users.noreply.github.com>
2025-05-15 21:53:42 -0400

    Se agregó los botones y el bucle que los crea

--- calculadora.py
+++ calculadora.py
@@ -15,10 +15,27 @@ def interfaz_tkinter():
         background="#B6B6B6"
     )
 
-    pantalla.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+    pantalla.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=4, pady=4)
 
     botones_frame = Frame(root)
     botones_frame.grid(row=1, column=0, padx=5, pady=5)
+
+    botones = [
+                            ("C",0,2),("<-",0,3),                 
+        ("7",1,0),("8",1,1),("9",1,2),("/",1,3),
+        ("4",2,0),("5",2,1),("6",2,2),("*",2,3),
+        ("1",3,0),("2",3,1),("3",3,2),("-",3,3),
+                  ("0",4,1),          ("=",4,3)
+    ]
+
+    for (num, x, y) in botones:
+        boton = Button(
+            botones_frame,
+            text=num,
+            font=("Consolas",14),
+            #command=lambda t=num: click_boton(t)
+        )
+        boton.grid(row=x, column=y, padx=2, pady=2)
 
     root.mainloop()
 
