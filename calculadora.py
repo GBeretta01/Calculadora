848ac30486c882c41fdf6307f252dd0e2c356358
Beretta <95521297+GBeretta01@users.noreply.github.com>
2025-05-15 23:01:59 -0400

    Corrección de colores (V1.0.3)
    * Se agregó los botones y el bucle que los crea
    
    * & D:/cppcompiler/ucrt64/bin/python.exe c:/Users/Gregory/Documents/calculadora_git/calculadora.py
    
    * Versión: 1.0.3. Se corrigieron los colores, lista para realizar funciones básicas

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,11 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import Tk, Frame, Label, Button
 
 def interfaz_tkinter():
     root = Tk()
-    root.title("Calculadora V1.0.0")
+    root.title("Calculadora V1.0.3")
     root.resizable(0,0)
+    root.configure(background="#525252", bg="#525252")
 
     pantalla = Entry(
         root,
@@ -12,13 +13,35 @@ def interfaz_tkinter():
         justify="right",
         bd = 10,
         insertwidth=4,
-        background="#B6B6B6"
+        background="#525252"
     )
 
     pantalla.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=4, pady=4)
 
     botones_frame = Frame(root)
-    botones_frame.grid(row=1, column=0, padx=5, pady=5)
+    botones_frame.grid(row=1, column=0, padx=4, pady=4, sticky="nsew", columnspan=4)
+    botones_frame.configure(background="#A3A3A3")
+
+    botones = [
+                            ("C",0,2),("<-",0,3),                 
+        ("7",1,0),("8",1,1),("9",1,2),("/",1,3),
+        ("4",2,0),("5",2,1),("6",2,2),("*",2,3),
+        ("1",3,0),("2",3,1),("3",3,2),("-",3,3),
+                  ("0",4,1),          ("=",4,3)
+    ]
+
+    for i in range(4):  
+        botones_frame.grid_columnconfigure(i, weight=1)  
+
+    for (num, x, y) in botones:
+        boton = Button(
+            botones_frame,
+            text=num,
+            font=("Consolas",14),
+            #command=lambda t=num: click_boton(t)
+            background="#525252"
+        )
+        boton.grid(row=x, column=y, padx=2, pady=2, sticky="nsew")
 
     botones = [
                             ("C",0,2),("<-",0,3),                 
